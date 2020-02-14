7a0563c1a34e9093778e84722e80dfbd86cfb3b2
Joe Hermaszewski <git@monoid.al>
2020-02-15 00:08:03 +0800

    Hyperlink handling

--- tmux.h
+++ tmux.h
@@ -276,6 +276,8 @@ enum tty_code_code {
 	TTYC_EL1,
 	TTYC_ENACS,
 	TTYC_FSL,
+	TTYC_HLS,
+	TTYC_HLR,
 	TTYC_HOME,
 	TTYC_HPA,
 	TTYC_ICH,
@@ -636,6 +638,7 @@ struct grid_cell {
 	int			fg;
 	int			bg;
 	int			us;
+	u_int			link;
 } __packed;
 struct grid_cell_entry {
 	u_char			flags;
@@ -676,6 +679,14 @@ struct grid {
 
 	struct grid_line	*linedata;
 };
+
+/* Hyperlink tree entry. */
+struct hyperlink {
+	u_int			 id;
+	const char		*link;
+	RB_ENTRY(hyperlink)	 entry;
+};
+RB_HEAD(hyperlinks, hyperlink);
 
 /* Style alignment. */
 enum style_align {
@@ -2195,6 +2206,11 @@ void	 file_push(struct client_file *);
 extern struct tmuxproc *server_proc;
 extern struct clients clients;
 extern struct cmd_find_state marked_pane;
+extern struct hyperlinks hyperlinks;
+extern u_int next_hyperlink;
+int	 server_cmp_hyperlink(struct hyperlink *, struct hyperlink *);
+struct hyperlink *server_get_hyperlink(u_int);
+RB_PROTOTYPE(hyperlinks, hyperlink, entry, server_cmp_hyperlink);
 void	 server_set_marked(struct session *, struct winlink *,
 	     struct window_pane *);
 void	 server_clear_marked(void);
